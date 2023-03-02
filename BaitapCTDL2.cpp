639bc041ac62ebebedba6104f6a75a317f00e6b1
AnPhamSF <125716057+AnPhamSF@users.noreply.github.com>
2023-03-02 15:12:08 +0700

    Add files via upload

---
+++ BaitapCTDL2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+long Tich(int n)
+{
+    if(n <= 1)
+        return 1;
+    return Tich(n - 1)*n;
+}
+
+int main()
+{
+    int n, ;
+    printf("\n Nhap n = ");
+    scanf("%d", &n);
+
+    T = Tich(n);
+    printf("T = %d", T);
+
+    getch();
+    return 0;
+}
